f51ecdc46ab126374496780301ff3bfaf4869aec
Dmitry Kravtsov <idkravitz@gmail.com>
2016-02-03 17:52:52 +1000

    Add OutputFid to Task model

--- model/model.go
+++ model/model.go
@@ -2,52 +2,54 @@ package model
 
 import (
 	"time"
+
 	"gopkg.in/mgo.v2/bson"
-	)
+)
 
 const (
 	TASK_STATUS_PENDING = "pending"
-	TASK_STATUS_DONE = "done"
+	TASK_STATUS_DONE    = "done"
 )
 
 type User struct {
 	Username string
-	Email string
+	Email    string
 	Password []byte
-	Dor time.Time
-	Banned bool
+	Dor      time.Time
+	Banned   bool
 }
 
 type Session struct {
-	Sid string
-	Username string
+	Sid       string
+	Username  string
 	CreatedAt time.Time
 }
 
 type FileDescription struct {
-	Filename string
+	Filename       string
 	Owner_Username string
 }
 
 type FileShortMeta struct {
-	Md5 string
-	Id string
-	Filename string
-	Size int
+	Md5        string
+	Id         string
+	Filename   string
+	Size       int
 	UploadDate time.Time
 }
 
 type TaskMsg struct {
-	TaskId bson.ObjectId
-	DataFid string
+	TaskId     bson.ObjectId
+	DataFid    string
 	ControlFid string
 }
 
 type Task struct {
-	Id bson.ObjectId `json:"id" bson:"_id,omitempty"`
-	Output string
-	Status string
-	Owner string
-	DataFid string
+	Id         bson.ObjectId `json:"id" bson:"_id,omitempty"`
+	Output     string
+	Status     string
+	Owner      string
+	DataFid    string
+	OutputFid  string `json:"output_fid" bson:"output_fid,omitempty"`
 	ControlFid string
 }

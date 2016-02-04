e19871ab2e7b2dc21d9f86238fcdcd11e4b44f3e
Dmitry Kravtsov <idkravitz@gmail.com>
2016-02-04 17:52:49 +1000

    Add raw handlers

--- web/web.go
+++ web/web.go
@@ -1,13 +1,14 @@
 package web
 
 import (
+	"encoding/json"
 	"io"
-	"os"
 	"log"
 	"net/http"
-	"encoding/json"
+	"os"
+
 	"gopkg.in/mgo.v2/bson"
-	)
+)
 
 /*
 Ideas for future:
@@ -16,10 +17,11 @@ Ideas for future:
 3. Copy/Close db sessions for mongo
 */
 
+// JSONApiHandler defines handler, that should be used to construct JSON API
 type JSONApiHandler func(bson.M, *http.Request)
 
 func (h JSONApiHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	var response bson.M = bson.M{}
+	response := bson.M{}
 	w.Header().Set("Content-type", "application/json")
 	log.Println(req.URL.Path)
 	req.ParseMultipartForm(100 << 20)
@@ -29,26 +31,33 @@ func (h JSONApiHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	}
 	h(response, req)
 	j, err2 := json.Marshal(response)
-	if err2 != nil { log.Fatal(err2) }
+	if err2 != nil {
+		log.Fatal(err2)
+	}
 	w.Write(j)
 }
 
-type ApiBuilder struct {
+// ApiBuilder is a wrapper for default mux, that allows to build API easily
+type APIBuilder struct {
 	mux *http.ServeMux
 }
 
-func NewApiBuilder() *ApiBuilder {
-	apiBuilder := &ApiBuilder{
+func NewApiBuilder() *APIBuilder {
+	apiBuilder := &APIBuilder{
 		mux: http.NewServeMux(),
 	}
 	return apiBuilder
 }
 
-func (b *ApiBuilder) HandleJson(pattern string, h JSONApiHandler) {
+func (b *APIBuilder) HandleJson(pattern string, h JSONApiHandler) {
 	b.mux.Handle(pattern, h)
 }
 
-func (b *ApiBuilder) AddStaticDir(dir string) {
+func (b *APIBuilder) HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+	b.mux.HandleFunc(pattern, handler)
+}
+
+func (b *APIBuilder) AddStaticDir(dir string) {
 	b.mux.HandleFunc(dir, staticGetter)
 }
 
@@ -64,17 +73,17 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 func staticGetter(w http.ResponseWriter, r *http.Request) {
 	var resource string = r.URL.Path
 	var staticRoot string = "./www/"
-	var fullResource string = staticRoot + resource;
+	var fullResource string = staticRoot + resource
 	f, err := os.Open(fullResource)
 	if err != nil {
 		log.Println("Static file not found:", resource)
 		http.NotFound(w, r)
 		return
 	}
-	io.Copy(w, f)		
+	io.Copy(w, f)
 }
 
-func (b *ApiBuilder) Build() *http.ServeMux {
+func (b *APIBuilder) Build() *http.ServeMux {
 	b.mux.HandleFunc("/", indexHandler)
-	return b.mux;
+	return b.mux
 }
